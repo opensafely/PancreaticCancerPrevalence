d2409707314171023987ab0316fdbff98ae777d5
lemanska <a.lemanska@surrey.ac.uk>
2023-02-15 17:15:18 +0000

    join ethnicity

--- analysis/study_definition_ethnicity.py
+++ analysis/study_definition_ethnicity.py
@@ -14,38 +14,49 @@ study = StudyDefinition(
     index_date=end_date,
     population=patients.all(),
 
-    ethnicity=patients.categorised_as(
-        {
-            "Missing": "DEFAULT",
-            "White": """ ethnicity_code=1 """,
-            "Mixed": """ ethnicity_code=2 """,
-            "South Asian": """ ethnicity_code=3 """,
-            "Black": """ ethnicity_code=4 """,
-            "Other": """ ethnicity_code=5 """,
-        },
+    ethnicity=patients.with_these_clinical_events(
+        ethnicity_codes,
+        returning="category",
+        find_last_match_in_period=True,
+        include_date_of_match=False,
         return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "Missing": 0.4,
-                    "White": 0.2,
-                    "Mixed": 0.1,
-                    "South Asian": 0.1,
-                    "Black": 0.1,
-                    "Other": 0.1,
-                }
-            },
-        },
-
-        ethnicity_code=patients.with_these_clinical_events(
-            ethnicity_codes,
-            returning="category",
-            find_last_match_in_period=True,
-            on_or_before="index_date",
-            return_expectations={
-            "category": {"ratios": {"1": 0.4, "2": 0.4, "3": 0.2, "4":0.2,"5": 0.2}},
+            "category": {"ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}},
             "incidence": 0.75,
-            },
-        ),
-    )
+        },
+    ),
+
+    # ethnicity=patients.categorised_as(
+    #     {
+    #         "Missing": "DEFAULT",
+    #         "White": """ ethnicity_code=1 """,
+    #         "Mixed": """ ethnicity_code=2 """,
+    #         "South Asian": """ ethnicity_code=3 """,
+    #         "Black": """ ethnicity_code=4 """,
+    #         "Other": """ ethnicity_code=5 """,
+    #     },
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {
+    #             "ratios": {
+    #                 "Missing": 0.4,
+    #                 "White": 0.2,
+    #                 "Mixed": 0.1,
+    #                 "South Asian": 0.1,
+    #                 "Black": 0.1,
+    #                 "Other": 0.1,
+    #             }
+    #         },
+    #     },
+
+    #     ethnicity_code=patients.with_these_clinical_events(
+    #         ethnicity_codes,
+    #         returning="category",
+    #         find_last_match_in_period=True,
+    #         on_or_before="index_date",
+    #         return_expectations={
+    #         "category": {"ratios": {"1": 0.4, "2": 0.4, "3": 0.2, "4":0.2,"5": 0.2}},
+    #         "incidence": 0.75,
+    #         },
+    #     ),
+    # )
 )
