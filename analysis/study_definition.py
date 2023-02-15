b10d4d1e3e97a8d4bdbc2079168e1b66ba0d1dae
lemanska <a.lemanska@surrey.ac.uk>
2023-02-15 17:59:52 +0000

    ethn cats

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -94,15 +94,36 @@ study = StudyDefinition(
     #         },
     #     ),
     # ),
-    ethnicity=patients.with_these_clinical_events(
-        ethnicity_codes,
-        returning="category",
-        find_last_match_in_period=True,
-        include_date_of_match=False,
+    ethnicity=patients.categorised_as(
+        {
+            "White": """ ethnicity_code=1 """,
+            "Mixed": """ ethnicity_code=2 """,
+            "Asian": """ ethnicity_code=3 """,
+            "Black": """ ethnicity_code=4 """,
+            "Chinese": """ ethnicity_code=5 """,
+        },
         return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "White": 0.2,
+                    "Mixed": 0.2,
+                    "Asian": 0.2,
+                    "Black": 0.2,
+                    "Chinese": 0.2,
+                }
+            },
+        },
+        ethnicity_code=patients.with_these_clinical_events(
+            ethnicity_codes,
+            returning="category",
+            find_last_match_in_period=True,
+            include_date_of_match=False,
+            return_expectations={
             "category": {"ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}},
             "incidence": 0.75,
-        },
+            },
+        ),
     ),
     imd_cat=patients.categorised_as(
         {
